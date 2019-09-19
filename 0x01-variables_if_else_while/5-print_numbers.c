d11b8ac5bd463fcdbc4b5114826bc90611c4f225
matxa <matxa21@gmail.com>
2019-09-19 18:18:25 +0000

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+int main(void)
+{
+	int count;
+
+	for (count = 0; count < 10; count++)
+		printf("%d ", count);
+	printf("\n");
+	return (0);
+}

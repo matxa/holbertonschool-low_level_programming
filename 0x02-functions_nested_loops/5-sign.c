2ffefc9e8dd3f99532d37b59983d84d4da5174e4
matxa <matxa21@gmail.com>
2019-09-24 17:39:41 +0000

    func & loops

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * print_sign - checks if c is lowercase
+ * @n: is a variables
+ * Return: 1 if c is lower case
+ *
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n > 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}

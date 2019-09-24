7e0fab7d5eea62bbbdc77940ab2e2a902b158fba
matxa <matxa21@gmail.com>
2019-09-24 15:31:23 +0000

    func & loops

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _islower - checks if c is lowercase
+ * @c is a variables
+ * Return 1 is c is lower case
+ * Return: Always 0 (Success)
+ */
+
+int _islower(int c)
+{
+
+	if (_islower(c))
+	{
+		return (1);
+	}
+	return (0);
+}

1dfd70ae1622e18b0d5562b870aa3213d60bd617
matxa <matxa21@gmail.com>
2019-09-25 17:53:42 +0000

    stressing

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,13 +1,45 @@
 #include "holberton.h"
 
 /**
- * jack_bauer - 24h format
+ * times_table - 9 times table
  *
- * Return: time
+ * Return: times table
  *
  */
 
 void times_table(void)
 {
-	
+	int hori, verti, total;
+
+	for (verti = 0; verti <= 9; verti++)
+	{
+		for (hori = 0; hori <= 9; hori++)
+		{
+			total = hori * verti;
+			if (total < 10)
+			{
+				if (hori != 0)
+				{
+					if (hori != 10)
+					{
+						_putchar(',');
+					}
+					_putchar(' ');
+					_putchar(' ');
+				}
+				_putchar(total % 10 + '0');
+			}
+			else if (total >= 10)
+			{
+				if (hori != 10)
+				{
+					_putchar(',');
+				}
+				_putchar(' ');
+				_putchar(total / 10 + '0');
+				_putchar(total % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
 }

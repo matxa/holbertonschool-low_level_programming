ff41bbc5e90db0188e1ca8cc84b6c080f8c49610
matxa <matxa21@gmail.com>
2019-09-26 17:12:20 +0000

    more func

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,14 +2,25 @@
 
 /**
  * print_line - prints lines
- *
+ * @n: num of lines
  * Return: lines ( __ )
  */
 
 void print_line(int n)
 {
+	if (n < 0)
+	{
+		_putchar('\n');
+	}
 	if (n > 0)
 	{
-		_putchar(95 * n);
+		for (; n > 0; n--)
+		{
+			_putchar(95);
+		}
+	}
+	if (n == 0)
+	{
+		_putchar('\n');
 	}
 }

0d173b02fb57d7cc88526ab00207e4d861edcef4
matxa <matxa21@gmail.com>
2019-09-26 20:05:35 +0000

    more func

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if (n % 3 != 0 && n % 5 != 0)
+		{
+			printf("%d ", n);
+		}
+		if (n % 3 == 0 && n % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (n % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (n % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+	}
+	putchar('\n');
+	return (0);
+}

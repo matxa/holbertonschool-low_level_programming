f768ac7498ed1a92579ebfecf66a01438a3a61ec
matxa <matxa21@gmail.com>
2019-09-26 13:22:08 +0000

    more func

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _islower - checks if c is lowercase
+ * @c: is a variables
+ * Return: 1 if c is lower case
+ *
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	return (0);
+}

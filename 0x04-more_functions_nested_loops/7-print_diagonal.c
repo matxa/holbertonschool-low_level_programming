c22de150d92406d45fcc226c50ebdcbcd8203cd8
matxa <matxa21@gmail.com>
2019-09-26 17:43:08 +0000

    more func

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,15 +10,15 @@ void print_diagonal(int n)
 {
 	int a, b;
 
-	for(a = 1; a <= n; a++)
+	for (a = 1; a <= n; a++)
 	{
-		for(b = 2; b <= a; b++)
+		for (b = 2; b <= a; b++)
 		{
 			_putchar(' ');
 		}
 		_putchar('\\');
 		_putchar('\n');
 	}
-	if(n < 1)
+	if (n < 1)
 		_putchar('\n');
 }

238813fbcf4012f432e1a33af34c7b9527403990
matxa <matxa21@gmail.com>
2019-09-26 20:13:18 +0000

    more func

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,19 +15,23 @@ int main(void)
 	{
 		if (n % 3 != 0 && n % 5 != 0)
 		{
-			printf("%d ", n);
+			printf("%d", n);
 		}
 		else if (n % 3 == 0 && n % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else if (n % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else if (n % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
+		}
+		if (n < 100)
+		{
+			printf(" ");
 		}
 	}
 	putchar('\n');

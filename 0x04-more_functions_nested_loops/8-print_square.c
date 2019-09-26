c9d2eb9d80aa0af5de5ac698ab95c39a26a28314
matxa <matxa21@gmail.com>
2019-09-26 19:07:06 +0000

    more func

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,6 +10,14 @@ void print_square(int size)
 {
 	int h, v;
 
+	if (size < 0)
+	{
+		_putchar('\n');
+	}
+	if (size == 0)
+	{
+		_putchar('\n');
+	}
 	for (v = 1; v <= size; v++)
 	{
 		for (h = 1; h <= size; h++)
@@ -17,13 +25,5 @@ void print_square(int size)
 			_putchar(35);
 		}
 		_putchar('\n');
-		if (size < 0)
-		{
-			_putchar('\n');
-		}
-		if (size == 0)
-		{
-			_putchar('\n');
-		}
 	}
 }

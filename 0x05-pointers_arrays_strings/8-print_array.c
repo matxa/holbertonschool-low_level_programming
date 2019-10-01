ca61876f29d2162d702991483d0d6aa6f0269878
matxa <matxa21@gmail.com>
2019-10-01 18:24:32 +0000

    pointers

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,12 +1,19 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
- *_puts - shows the pointer of the var
+ *print_array - prints array sepearted by comma and space
  *@a: pointer
  *@n: int
  */
 
 void print_array(int *a, int n)
 {
-	
+	int i = 0;
+
+	n = sizeof(*a);
+
+	for (; i <= n; i++)
+		printf(i ? ", %d" : "%d", a[i]);
+	putchar('\n');
 }

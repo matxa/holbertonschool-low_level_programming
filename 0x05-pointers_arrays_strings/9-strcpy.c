9973f5dbe2db336f4da79fd2d008474f4f1e596e
matxa <matxa21@gmail.com>
2019-10-01 22:25:01 +0000

    pointers

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ **_strcpy - copy
+ *@dest: pointer
+ *@src: int
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	int n = 98;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+	return (dest);
+}

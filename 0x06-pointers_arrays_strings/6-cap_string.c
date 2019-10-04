b95835b91d3df0784a37abbf4bec467d15bc52b7
matxa <matxa21@gmail.com>
2019-10-04 18:16:40 +0000

    pointer

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * cap_string - uppercase first letter of string
+ * @s: is a variables
+ * Return: s
+ *
+ */
+
+char *cap_string(char *s)
+{
+
+	int i = 1;
+	int j;
+
+	char *sep = ",:.!? \t\n,)\"({}";
+
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (sep[j] != '\0')
+		{
+			if (s[i] >= 'a' && s[i] <= 'z')
+			{
+				if (s[i - 1] == sep[j])
+					s[i] -= 'a' - 'A';
+			}
+			j++;
+		}
+		i++;
+	}
+	return (s);
+}

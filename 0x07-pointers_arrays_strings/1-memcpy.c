b961dc2d1654d05bbab79b93fe34303cd03538e1
matxa <matxa21@gmail.com>
2019-10-08 23:02:28 +0000

    Num 0

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,14 +8,7 @@
  *Return: (b)
  */
 
-char *_memset(char *s, char b, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	char *temp_p = s;
-	unsigned int i = 0;
-
-	for (; i != n; ++i)
-	{
-		temp_p[i] = b;
-	}
-	return (s);
+	
 }

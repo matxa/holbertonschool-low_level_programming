75775e5896598e0a56a2733480ae58cc3414ca56
matxa <matxa21@gmail.com>
2019-10-08 22:34:38 +0000

    Num 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,2 +1,21 @@
 #include "holberton.h"
 
+/**
+ **_memset - function that fills memory with a constant byte
+ *@s: pointer
+ *@b: char var
+ *@n: int var
+ *Return: (b)
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *p = (char *)s;
+	unsigned int i = 0;
+
+	for (; i != n; ++i)
+	{
+		p[i] = b;
+	}
+	return (s);
+}

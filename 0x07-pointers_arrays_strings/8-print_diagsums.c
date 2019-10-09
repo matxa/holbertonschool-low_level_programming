57fced3522bf7e1dbaee058d3419444833794897
matxa <matxa21@gmail.com>
2019-10-09 08:17:25 +0000

    set str

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *print_diagsums - iterates through a 2d array using putchar;
+ *@a: pointer
+ *
+ *Return: (s)
+ */
+
+void print_diagsums(int *a, int size)
+{
+
+}
---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ *set_string - iterates through a 2d array using putchar;
+ *@s: pointer to a pointer
+ *@to: pointer
+ *Return: (s)
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}

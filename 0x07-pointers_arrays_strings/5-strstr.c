5ceabfaa2d90fe74f1065c004c893da92e10b358
matxa <matxa21@gmail.com>
2019-10-09 06:01:51 +0000

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -14,7 +14,7 @@ char *_strstr(char *haystack, char *needle)
 	{
 		if (*haystack == *needle)
 		{
-			if (!_strcmp(haystack, needle))
+			if (_strcmp(haystack, needle) == 0)
 				return ((char *)haystack);
 		}
 		haystack++;
@@ -42,4 +42,24 @@ int _strcmp(char *s1, char *s2)
 			equal = *(s1 + i) - *(s2 + i);
 	}
 	return (equal);
+}
+
+#include "holberton.h"
+
+/**
+ *_strlen - shows the pointer of the var
+ *@s: var for p and init
+ *Return: (length)
+ */
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	return (length);
 }

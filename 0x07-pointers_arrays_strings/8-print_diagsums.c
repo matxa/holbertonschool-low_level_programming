73d61df27725ee07316a6f7ed6b28d761d7faa97
matxa <matxa21@gmail.com>
2019-10-09 08:34:08 +0000

    sum

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -4,11 +4,26 @@
 /**
  *print_diagsums - iterates through a 2d array using putchar;
  *@a: pointer
- *
+ *@size: int var
  *Return: (s)
  */
 
 void print_diagsums(int *a, int size)
 {
+	int i, val, sum_one, sum_two = 0;
 
+	while (i < (size * size))
+	{
+		val = a[i];
+		sum_one = sum_one + val;
+		i = i + size + 1;
+	}
+	i = size - 1;
+	while (i < ((size * size) - 1))
+	{
+		val = a[i];
+		sum_two = sum_two + val;
+		i = i + (size - 1);
+	}
+	printf("%d, %d\n", sum_one, sum_two);
 }

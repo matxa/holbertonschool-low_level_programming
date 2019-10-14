39736b5a4eadf70ef7523a143fb69ae2eb9fab62
matxa <matxa21@gmail.com>
2019-10-14 01:21:15 +0000

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - num of argumemnts
+ *@argc: num of arg
+ *@argv: the arg
+ *Return: return num of args
+ *
+ */
+
+int main(int argc, char **argv __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}

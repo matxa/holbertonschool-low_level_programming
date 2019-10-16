e2f885c02b35b5d7eaf9cee05c0367326c18f6d2
matxa <matxa21@gmail.com>
2019-10-16 12:46:02 +0000

    add malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * *create_array - creates array
+ * @size: size
+ * @c: point char
+ * Return: (0);
+ *
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *hold_c;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	hold_c = malloc(sizeof(unsigned int) * size);
+
+	for (i = 0; i < size; i++)
+	{
+		hold_c[i] = c;
+	}
+	return (hold_c);
+}

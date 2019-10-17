f83879599860d16b4335742d13a63a185bd796fe
matxa <matxa21@gmail.com>
2019-10-17 03:36:48 +0000

    last puhs

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - free grid
+ * @grid: pointer to a pointer
+ * @height: height
+ * Return: free
+ */
+
+void free_grid(int **grid, int height)
+{
+
+}
---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - args to str
+ * @ac: ac
+ * @av: av
+ * Return: null
+ *
+ */
+
+char *argstostr(int ac, char **av)
+{
+
+}

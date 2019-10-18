76cf10129f3d753acdcc74b452c4cc8118ed3631
matxa <matxa21@gmail.com>
2019-10-18 20:21:43 +0000

    check for free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -24,11 +24,11 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (ptr == NULL)
 	{
 		str = malloc(new_size);
-                return (str);
+		return (str);
 	}
 	if (new_size == 0 && ptr != NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
 	return (0);
 }

47cd4f4356fc8c7f3922f33432d4c312fd84c311
matxa <matxa21@gmail.com>
2019-10-18 19:50:48 +0000

    _calloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,12 +11,32 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int size = strnlen(s2, n);
-	char *concat = malloc(strlen(s1) + strlen(s2));
-	if (size != n)
-	{
-		memset(s1 + size, '\0', n - size);
-	}
-	concat = memcpy(s1, s2, size);
-	return (concat);
+	char *temp;
+	size_t len1, len2;
+	size_t a, b;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	for (len1 = 0; s1[len1] != '\0'; len1++)
+	{}
+	for (len2 = 0; s2[len2] != '\0'; len2++)
+	{}
+
+	if (n >= len2)
+		n = len2;
+
+	temp = malloc(sizeof(char) * (len1 + n + 1));
+	if (!temp)
+		return (NULL);
+
+	for (a = 0; s1[a] < len1; a++)
+		temp[a] = s1[a];
+	for (b = 0; s2[b] < len2; b++)
+                temp[a + b] = s1[a];
+	temp[a + n + '\0'];
+
+	return (temp + '\0');
 }
--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,5 +11,16 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	
+	char *temp;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	temp = malloc(nmemb * size);
+	if (temp == NULL)
+		return (NULL);
+	for (i = 0; i < (nmemb * size); i++)
+		temp[i] = 0;
+
+	return (temp);
 }

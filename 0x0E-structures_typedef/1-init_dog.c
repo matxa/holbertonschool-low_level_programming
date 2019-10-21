8ef040c59d1d901e45d310f8a5b59a3a04314c44
matxa <matxa21@gmail.com>
2019-10-21 18:03:01 +0000

    add proto to dog.h

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,11 +1,21 @@
-#include "holberton.h"
+#include <stdio.h>
+#include "dog.h"
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 /**
- *
- *
- *
- *
- *
+ * init_dog - initialize dog func
+ * struct dog - info about dog
+ * @d: dog type pointer
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
  */
 
-void print_dog(struct dog *d);
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d -> name = name;
+	d -> age = age;
+	d -> owner = owner;
+}

b9d6c55a8c5c12445173eabfcfb29c8cce1d6436
matxa <matxa21@gmail.com>
2019-10-21 21:30:34 +0000

    pointer redi

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * new_dog - func to make new dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ * Return: (new_dog)
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
+}

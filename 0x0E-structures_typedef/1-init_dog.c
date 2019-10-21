28ed74e1879c2b6798e6500946471dff0f307429
matxa <matxa21@gmail.com>
2019-10-21 21:58:55 +0000

    if

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
@@ -15,10 +16,11 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+	{
+		exit(98);
+	}
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
-
-	if (d == NULL)
-	{}
 }

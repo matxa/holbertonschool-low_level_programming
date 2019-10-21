637764b2067b9b3d4adcd0043cc640b0d0bdfc14
matxa <matxa21@gmail.com>
2019-10-21 17:08:15 +0000

    prepro

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef ABSOLUTE_VALUE
+#define ABSOLUTE_VALUE
+
+#define ABS(i) ((i < 0) ? -i : i)
+
+#endif
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,6 @@
+#ifndef SUM_TWO_NUMS
+#define SUM_TWO_NUMS
+
+#define SUM(x, y) (x + y)
+
+#endif

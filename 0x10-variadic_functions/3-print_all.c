fb539fd5468ac8add9f21d3eaba2bc3ef6e62fca
matxa <matxa21@gmail.com>
2019-10-27 23:31:04 +0000

    delete else stat

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -10,46 +10,44 @@
 
 void print_all(const char * const format, ...)
 {
-	int i;
+	unsigned int i, flag;
 	char *str;
 
 	va_list pa;
 
 	va_start(pa, format);
 
+	flag = 0;
 	i = 0;
 	while (format[i] != '\0')
 	{
+		if (flag)
+			printf(", ");
 		switch (format[i])
 		{
 		case 'c':
-			printf("%c, ", va_arg(pa, int));
-			i++;
+			printf("%c", va_arg(pa, int));
 			break;
 		case 'i':
-			printf("%i, ", va_arg(pa, int));
-			i++;
+			printf("%i", va_arg(pa, int));
 			break;
 		case 'f':
-			printf("%f, ", va_arg(pa, double));
-			i++;
+			printf("%f", va_arg(pa, double));
 			break;
 		case 's':
 			str = va_arg(pa, char*);
 			if (str)
-			{
 				printf("%s", str);
-			}
-			else
-			{
-				printf("(nil)");
-			}
-			i++;
+			break;
+			printf("(nil)");
 			break;
 		default:
+			flag = 0;
 			i++;
-			break;
+			continue;
 		}
+		flag = 1;
+		i++;
 	}
 	printf("\n");
 	va_end(pa);

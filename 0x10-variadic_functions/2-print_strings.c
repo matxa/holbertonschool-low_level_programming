1ff5760516f0f2901046528e62f3e04c9c559b85
matxa <matxa21@gmail.com>
2019-10-27 23:12:54 +0000

    move around func

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -18,17 +18,17 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
+		arg = va_arg(ps, char*);
 		if (arg)
 		{
-			arg = va_arg(ps, char*);
 			printf("%s", arg);
-			if (separator && i < (n - 1))
-				printf("%s", separator);
 		}
 		else
 		{
 			printf("(nil)");
 		}
+		if (separator && i < (n - 1))
+			printf("%s", separator);
 	}
 	va_end(ps);
 

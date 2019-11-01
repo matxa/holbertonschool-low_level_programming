a331e21023234c97a88705ac206550955257a75c
matxa <matxa21@gmail.com>
2019-11-01 15:56:30 +0000

    added header

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
+#include "lists.h"
 
 /**
  * before_main - prints string before main exec
  *
  */
 
-void before_main(void) __attribute__((constructor));
 void before_main(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }

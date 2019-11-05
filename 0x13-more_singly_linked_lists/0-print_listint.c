a786f5ac5902ece982be5202756cff627983f95b
matxa <matxa21@gmail.com>
2019-11-05 16:42:04 +0000

    delete if(temp->n)

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,12 +11,9 @@ size_t print_listint(const listint_t *h)
 	const listint_t *temp_h = h;
 	int num_of_elem = 0;
 
-	while (temp_h != NULL)
+	while (temp_h)
 	{
-		if (temp_h->n || temp_h->n == 0)
-		{
-			printf("%d\n", temp_h->n);
-		}
+		printf("%d\n", temp_h->n);
 		temp_h = temp_h->next;
 		num_of_elem++;
 	}

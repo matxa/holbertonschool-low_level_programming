abfe581857578b0703220e19a6360666f95efaba
matxa <matxa21@gmail.com>
2019-11-06 00:04:05 +0000

    che

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -22,7 +22,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		free(temp);
 	}
 
-	for (i = 0; temp != NULL && i < index -1; i++)
+	for (i = 0; temp != NULL && i < index - 1; i++)
 	{
 		temp = temp->next;
 	}

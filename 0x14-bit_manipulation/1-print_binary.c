d67f39b044190692438bf88c54dd92b918901f73
matxa <matxa21@gmail.com>
2019-11-08 03:07:33 +0000

    rec

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,18 +8,9 @@
 
 void print_binary(unsigned long int n)
 {
-	unsigned long int range = 2147483648;
-
-	while (range > 0)
+	if (n)
 	{
-		if ((range & n) == 0)
-		{
-			_putchar('0');
-		}
-		else
-		{
-			_putchar('1');
-		}
-		range = range >> 1;
+		print_binary(n >> 1);
+		_putchar((n & 1) ? '1' : '0');
 	}
 }

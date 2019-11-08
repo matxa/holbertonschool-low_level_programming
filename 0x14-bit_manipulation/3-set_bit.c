2ce88c1531d66c4963ad647ff581838336a2583f
matxa <matxa21@gmail.com>
2019-11-08 18:31:56 +0000

    set

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,9 +9,10 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > 63)
+	if (sizeof(*n) * 8)
 	{
-		return (-1);
+		*n |= 1 << index;
+		return (1);
 	}
-	return (*n |= 1 << index);
+	return (-1);
 }

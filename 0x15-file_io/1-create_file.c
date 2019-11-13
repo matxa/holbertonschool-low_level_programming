b46d4ece54955a610449bbd1695a6c96e9fdb088
matxa <matxa21@gmail.com>
2019-11-13 22:45:07 +0000

    first com

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,32 +10,24 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd_create;
-	int bytes;
-	int write_var;
-	char c;
+	int write_len;
+	int len = 0;
 
 	if (filename == NULL)
-	{
 		return (-1);
-	}
+
+	if (text_content == NULL)
+		text_content = " ";
 
 	fd_create = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
-	if (fd_create == (-1))
-	{
-		return (-1);
-	}
-
-	while ((bytes = read(fd_create, &c, sizeof(c))) > 0)
-	{
-		write_var = write(fd_create, text_content, sizeof(c));
-	}
-
-
-	if (bytes == (-1))
+	if (fd_create == -1)
 		return (-1);
 
-	if (write_var == (-1))
+	while (text_content[len] != '\0')
+		len++;
+
+	write_len = write(fd_create, text_content, len);
+	if (write_len == -1)
 		return (-1);
 
 	close(fd_create);

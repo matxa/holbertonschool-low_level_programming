8e288bb05d6f8b04add9f693f3b7df548b8342f1
matxa <matxa21@gmail.com>
2019-11-13 17:22:29 +0000

    sdas

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,28 +11,27 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd_read;
-	char buffer[sizeof(letters)];
-	size_t bytes;
-	char c;
-	size_t i = 0;
+	char *buffer;
+	ssize_t read_count;
+	ssize_t write_count;
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
 
 	fd_read = open(filename, O_RDONLY);
-
-	if (fd_read == (-1) || filename == NULL)
-	{
+	if (fd_read == -1)
 		return (0);
-	}
 
-	while ((bytes = read(fd_read, &c, sizeof(c))) > 0)
-	{
-		write(fd_read, buffer, sizeof(char));
-		if (i < letters)
-		{
-			i++;
-			printf("%c", c);
-		}
-	}
+	read_count = read(fd_read, buffer, letters);
+	if (read_count == -1)
+		return (0);
 
+	write_count = write(1, buffer, read_count);
+	if (write_count == -1)
+		return (0);
+
+	free(buffer);
 	close(fd_read);
-	return (i);
+	return (write_count);
 }
--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,19 +11,32 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd_create;
 	int bytes;
+	int write_var;
 	char c;
 
-	fd_create = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (filename == NULL)
+	{
+		return (-1);
+	}
 
-	if (fd_create == (-1) || filename == NULL)
+	fd_create = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fd_create == (-1))
 	{
 		return (-1);
 	}
 
 	while ((bytes = read(fd_create, &c, sizeof(c))) > 0)
 	{
-		write(fd_create, text_content, sizeof(c));
+		write_var = write(fd_create, text_content, sizeof(c));
 	}
+
+
+	if (bytes == (-1))
+		return (-1);
+
+	if (write_var == (-1))
+		return (-1);
 
 	close(fd_create);
 	return (1);

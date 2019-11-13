793a39edc345f63179079836ca03fe4d97cb60e8
matxa <matxa21@gmail.com>
2019-11-13 22:49:38 +0000

    2nd com

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (text_content == NULL)
-		text_content = " ";
+		text_content = "";
 
 	fd_create = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd_create == -1)

caf908268170871310801c8b77e04421ba525ec6
matxa <matxa21@gmail.com>
2019-11-13 02:35:41 +0000

    updated everythin

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,27 +2,37 @@
 
 /**
 * read_textfile - read text file
-* @filename - filename
-* @letters - letters
+* @filename: filename
+* @letters: letters
 * Return: ssize_t
 *
 */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	char *buffer = malloc(sizeof(char) * letters);
+	int fd_read;
+	char buffer[sizeof(letters)];
+	size_t bytes;
+	char c;
+	size_t i = 0;
 
-	fd = open(filename, O_RDONLY);
+	fd_read = open(filename, O_RDONLY);
 
-	if(fd == (-1) || filename == NULL)
+	if (fd_read == (-1) || filename == NULL)
 	{
 		return (0);
 	}
 
-	read(fd, buffer, letters);
-	printf("%s", buffer);
+	while ((bytes = read(fd_read, &c, sizeof(c))) > 0)
+	{
+		write(fd_read, buffer, sizeof(char));
+		if (i < letters)
+		{
+			i++;
+			printf("%c", c);
+		}
+	}
 
-	close(fd);
-	return(letters);
+	close(fd_read);
+	return (i);
 }

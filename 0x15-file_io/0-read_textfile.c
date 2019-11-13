4cf67f7fda6b2bdb34dbbacf6dd3d73ef0751be5
matxa <matxa21@gmail.com>
2019-11-13 17:39:03 +0000

    sdas

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,7 +14,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *buffer;
 	ssize_t read_count;
 	ssize_t write_count;
-
+	ssize_t close_check;
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 		return (0);
@@ -28,10 +28,13 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	write_count = write(1, buffer, read_count);
-	if (write_count == -1 || write_count != read_count)
+	if (write_count == -1 ) /* || write_count != read_count) */
 		return (0);
 
-	close(fd_read);
+	close_check = close(fd_read);
+	if (close_check == -1)
+		return (0);
+
 	free(buffer);
 	return (write_count);
 }

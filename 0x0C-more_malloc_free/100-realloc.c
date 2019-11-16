67804a66d4580e310833ec3655541cac7ffd853e
matxa <matxa21@gmail.com>
2019-11-16 17:40:55 +0000

    re

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -12,11 +12,16 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
+	/* create a new pointer where we will store the value of */
+	/* the   the old pointer (ptr) */
 	char *new_ptr;
+	/*count - loop throught the index of each pointer to swap */
+	/* values between them copy whats in (ptr) into (new_ptr) */
 	unsigned int count;
 
 	if (ptr == NULL)
 	{
+		/*if ptr is NULL set malloc for new_ptr*/
 		new_ptr = malloc(new_size);
 		return (new_ptr);
 	}
@@ -29,15 +34,18 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (ptr);
 	/* malloc for new_ptr */
 	new_ptr = malloc(new_size);
+	/* check if new_ptr was successfully allocated */
 	if (new_ptr == NULL)
 	{
 		return (NULL);
 	}
-
+	/* increment count while count is less then old_size and new_size */
+	/* and copy whats inside of ptr to new_ptr */
 	for (count = 0; count < old_size && count < new_size; count++)
 	{
 		new_ptr[count] = ((char *)ptr)[count];
 	}
+	/* don't forget to free ptr */
 	free(ptr);
 	return (new_ptr);
 }

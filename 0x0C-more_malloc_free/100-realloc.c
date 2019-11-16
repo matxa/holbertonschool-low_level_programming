e9851d42fa48c5cf1ad08e272937c6a0c4cd079a
matxa <matxa21@gmail.com>
2019-11-16 16:45:46 +0000

    free memory and add new func

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -6,17 +6,18 @@
  * @ptr: void ptr
  * @old_size: old
  * @new_size: new
- * Return: new_size
+ * Return: void func
  *
  */
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *str;
+	char *new_ptr;
 
 	if (new_size > old_size)
 	{
-		ptr = malloc(new_size);
+		new_ptr = malloc(new_size);
+		free(new_ptr);
 		return (ptr);
 	}
 	if (new_size == old_size)
@@ -24,14 +25,50 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (ptr == NULL)
 	{
-		str = malloc(new_size);
-		free(ptr);
-		return (str);
+		new_ptr = malloc(new_size);
+		free(new_ptr);
+		return (ptr);
 	}
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
+
+	new_ptr = malloc(new_size);
+	_memcpy(new_ptr, ptr, old_size);
+	free(new_ptr);
+	free(ptr);
 	return (0);
+
+}
+
+/**
+* _getmin - gets the minimum of two numbers
+* @a: first num
+* @b: second num
+* Return: min
+*/
+
+int _getmin(unsigned int a, unsigned int b)
+{
+	return (a < b ? a : b);
+}
+
+/**
+ **_memcpy - function that fills memory with a constant byte
+ *@dest: pointer
+ *@src: char var
+ *@n: int var
+ *Return: (b)
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *temp_p = dest;
+	const char *s = src;
+
+	while (n--)
+		*temp_p++ = *s++;
+	return (dest);
 }

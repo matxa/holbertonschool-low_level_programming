bb1fc3798241bdf766574030523877edfbfe65b8
matxa <matxa21@gmail.com>
2020-05-03 00:13:51 -0400

    change if

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -19,8 +19,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 
 	new_node->n = n;
-	new_node->next = NULL;
-	new_node->prev = NULL;
 
 	if (idx == 0)
 	{

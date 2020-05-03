a25fa485037c4b80281ebb91fd3dbdc00d3d206d
matxa <matxa21@gmail.com>
2020-05-02 21:02:55 -0400

    testing

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -15,20 +15,18 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (new_node == NULL)
 		return (NULL);
 
-	new_node->n = n;
-	new_node->next = *head;
-
 	if (*head == NULL)
 	{
 		new_node->prev = NULL;
+		new_node->next = *head;
 		*head = new_node;
 		return (new_node);
 	}
 
-	if (*head != NULL)
-	{
-		new_node->prev = new_node;
-	}
+	new_node->n = n;
+	new_node->next = *head;
+	new_node->prev = new_node;
+
 	*head = new_node;
 	return (new_node);
 }

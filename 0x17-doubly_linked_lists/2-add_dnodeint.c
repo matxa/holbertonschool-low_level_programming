5d3394f86cc6420f943b8063c2696c2aaab81b27
matxa <matxa21@gmail.com>
2020-05-02 21:16:37 -0400

    fix betty

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -15,17 +15,12 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (new_node == NULL)
 		return (NULL);
 
-	if (*head == NULL)
-	{
-		new_node->prev = NULL;
-		new_node->next = *head;
-		*head = new_node;
-		return (new_node);
-	}
-
 	new_node->n = n;
 	new_node->next = *head;
-	new_node->prev = new_node;
+	new_node->prev = NULL;
+
+	if (*head != NULL)
+		(*head)->prev = new_node;
 
 	*head = new_node;
 	return (new_node);

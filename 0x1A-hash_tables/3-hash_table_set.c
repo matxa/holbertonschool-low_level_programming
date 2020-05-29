d726dbbd2accc58a0fbf3ccb4756d510296f90fc
matxa <matxa21@gmail.com>
2020-05-29 19:52:22 +0000

    hope

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,45 +1,45 @@
 #include "hash_tables.h"
 /**
-* hash_table_set - a function that adds an element to the hash table
-* @ht: is the hash table you want to add or update the key/value to
-* @key: is the key
-* @value: must be duplicated
-* Return: 1 if success else 0
-*
-*/
+ * hash_table_set - a function that adds an element to the hash table
+ * @ht: is the hash table you want to add or update the key/value to
+ * @key: is the key
+ * @value: must be duplicated
+ * Return: 1 if success else 0
+ *
+ */
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    unsigned long int index;
-    hash_node_t *temp;
-    hash_node_t *new_node = malloc(sizeof(hash_node_t));
+	unsigned long int index;
+	hash_node_t *temp;
+	hash_node_t *new_node = malloc(sizeof(hash_node_t));
 
-    if (ht == NULL ||  key == NULL || value == NULL)
-    {
-        return (0);
-    }
-    if (new_node == NULL)
-    {
-        return (0);
-    }
-    new_node->value = strdup(value);
-    new_node->next = NULL;
+	if (ht == NULL ||  key == NULL || value == NULL)
+	{
+		return (0);
+	}
+	if (new_node == NULL)
+	{
+		return (0);
+	}
+	new_node->value = strdup(value);
+	new_node->next = NULL;
 
-    index = key_index((const unsigned char *)key, ht->size);
+	index = key_index((const unsigned char *)key, ht->size);
 
-    if (ht->array[index] == NULL)
-    {
-        ht->array[index] = new_node;
-    }
-    else
-    {
-        temp = ht->array[index];
-        while (temp->next)
-        {
-            temp = temp->next;
-        }
-        temp->next = new_node;
-    }
+	if (ht->array[index] == NULL)
+	{
+		ht->array[index] = new_node;
+	}
+	else
+	{
+		temp = ht->array[index];
+		while (temp->next)
+		{
+			temp = temp->next;
+		}
+		temp->next = new_node;
+	}
 
-    return (1);
+	return (1);
 }

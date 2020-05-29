56fd593e1437b8ef0641fbde36efab76d6067e56
matxa <matxa21@gmail.com>
2020-05-29 11:44:13 -0400

    add more lib to header

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,8 +1,9 @@
 #ifndef HASH_TABLE_H
 #define HASH_TABLE_H
-
-#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdio.h>
 
 /**
  * struct hash_node_s - Node of a hash table

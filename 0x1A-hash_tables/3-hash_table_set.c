f04b5e873f93036620df63fd754baf3424b97739
matxa <matxa21@gmail.com>
2020-05-29 17:45:25 -0400

    testing

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -34,7 +34,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		if (strcmp(temp->key, key) == 0)
 		{
+			free(temp->value);
 			temp->value = strdup(value);
+			free(new_node);
+			free(temp->key);
 			return (1);
 		}
 		temp = temp->next;

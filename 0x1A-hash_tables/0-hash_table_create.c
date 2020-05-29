adf0d772f10548d0b3255dcbbfd8e076b3ddf2d1
matxa <matxa21@gmail.com>
2020-05-29 14:43:08 +0000

    testing malloc

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,17 +1,19 @@
 #include "hash_tables.h"
 /**
-* hash_table_create - function that creates a hash table
-* @size: the size of the array
-*
-*/
+ * hash_table_create - function that creates a hash table
+ * @size: the size of the array
+ * Return: pointer to created table
+ *
+ */
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *hash_table = malloc(sizeof(unsigned long int) * size);
-    if (hash_table != NULL)
-    {
-        return (hash_table);
-    }
+	hash_table_t *hash_table = malloc(sizeof(unsigned long int) * size);
 
-    return (NULL);
+	if (hash_table != NULL)
+	{
+		return (hash_table);
+	}
+
+	return (NULL);
 }

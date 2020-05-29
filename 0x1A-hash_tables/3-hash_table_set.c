a42f8464cae1ab690b503309a159dc83c55274f0
matxa <matxa21@gmail.com>
2020-05-29 17:03:14 -0400

    f

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,10 +12,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
 	hash_node_t *temp;
-	hash_node_t *new_node = malloc(sizeof(hash_node_t));
+	hash_node_t *new_node;
 
 	if (ht == NULL || strcmp(key, "") == 0 || value == NULL || key == NULL)
 		return (0);
+	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 	{
 		return (0);

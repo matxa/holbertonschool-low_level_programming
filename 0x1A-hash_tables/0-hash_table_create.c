85e68fddc61b1680540b4350cc674e4d21d44abb
matxa <matxa21@gmail.com>
2020-05-29 11:40:25 -0400

    change the sizeof

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,13 +11,18 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table;
 
+	if (!size)
+	{
+		return (NULL);
+	}
+
 	hash_table = malloc(sizeof(hash_table_t));
 	if (hash_table == NULL)
 	{
 		return (NULL);
 	}
 
-	hash_table->array = calloc(size, sizeof(hash_node_t *));
+	hash_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (hash_table->array == NULL)
 	{
 		free(hash_table);

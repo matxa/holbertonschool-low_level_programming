ee07ce5a6a68976ac05e83c29bfc85df582b0358
matxa <matxa21@gmail.com>
2020-05-29 12:52:25 -0400

    getting the index

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -14,7 +14,9 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 		return (NULL);
 	}
 
-	unsigned long int index = hash_djb2(key) % size;
+	unsigned long int index;
+
+	index = hash_djb2(key) % size;
 
 	return (index);
 }

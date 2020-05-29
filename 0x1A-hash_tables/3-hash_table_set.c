eeeb223e18d310d6a7d226e21b8847e814dc3227
matxa <matxa21@gmail.com>
2020-05-29 16:13:37 -0400

    fi

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -32,7 +32,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	else
 	{
 		temp = ht->array[index];
-		while (temp->next)
+		while (temp->next && strcmp(temp->next->key, key) != 0)
 		{
 			temp = temp->next;
 		}

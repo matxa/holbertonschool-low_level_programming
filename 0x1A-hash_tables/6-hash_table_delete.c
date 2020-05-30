110ea8b339232ce6df4833f589ecec4ad7dd1feb
matxa <matxa21@gmail.com>
2020-05-29 21:04:02 -0400

    f

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - a function that deletes a hash table
+ * @ht: HashTable
+ * Return: VOID
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *temp;
+	hash_node_t *cur_temp;
+	unsigned long int i;
+
+	if (ht == NULL)
+		return;
+	for (i = 0; i < ht->size; i++)
+	{
+		cur_temp = ht->array[i];
+		while (temp)
+		{
+			temp = cur_temp;
+			cur_temp = cur_temp->next;
+			free(temp->value);
+			free(temp->key);
+			free(temp);
+		}
+	}
+	free(ht->array);
+	free(ht);
+}

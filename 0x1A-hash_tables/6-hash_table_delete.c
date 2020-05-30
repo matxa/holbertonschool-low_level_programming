cb9d8509b9392e8207a97d2c6602687c16c78a9c
matxa <matxa21@gmail.com>
2020-05-29 21:11:21 -0400

    f

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -20,8 +20,8 @@ void hash_table_delete(hash_table_t *ht)
 		{
 			temp = cur_temp;
 			cur_temp = cur_temp->next;
+            free(temp->key);
 			free(temp->value);
-			free(temp->key);
 			free(temp);
 		}
 	}

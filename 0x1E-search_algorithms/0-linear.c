ad54e6f39316cd0d9d5f059dc2f0e08895e23ee4
matxa <42968545+matxa@users.noreply.github.com>
2020-08-25 02:12:48 -0400

    first set of the project

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,26 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - linear search function
+ * @array: array to iterate
+ * @size: size of the array
+ * @value: value to seach for in the array
+ * Return: index
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (array == NULL)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%zu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef S_ALGO
+#define S_ALGO
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif

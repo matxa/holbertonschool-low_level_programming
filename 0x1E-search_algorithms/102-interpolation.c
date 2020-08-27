ead4fd27f6823930179b585ac3f044dfd1b6b119
matxa <42968545+matxa@users.noreply.github.com>
2020-08-27 17:28:47 -0400

    add a awhile loop

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -13,30 +13,22 @@ int interpolation_search(int *array, size_t size, int value)
 	size_t pos;
 	size_t low = 0;
 	size_t high = size - 1;
-	size_t i;
-	size_t addiv;
+	size_t sub_pos;
 
 	if (array == NULL)
 		return (-1);
 
-	if (value > array[high])
-		return (-1);
-
-	pos = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
-	if (array[pos] == value)
+	while (low <= high && value >= array[low] && value <= array[high])
 	{
+		sub_pos = ((double)(high - low) / (array[high] - array[low]));
+		pos = low + (sub_pos * (value - array[low]));
 		printf("Value checked array[%zu] = [%d]\n", pos, array[pos]);
-		return (pos);
+		if (array[pos] == value)
+			return (pos);
+		if (value > array[pos])
+			low = pos + 1;
+		else
+			high = pos - 1;
 	}
-	else
-	{
-		for (i = pos; i < high; i++)
-		{
-			printf("Value checked array[%zu] = [%d]\n", i, array[i]);
-			if (array[i] == value)
-				return (i);
-		}
-	}
-
 	return (-1);
 }
